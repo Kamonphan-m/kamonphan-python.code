cab229ef1894d7173cba2213a9a6d6c59987960f
Kamonphan mongkonsin <kamonphan.m@ku.th>
2025-07-29 12:13:48 +0700

    week 04.

---
+++ assignments/week04/lab 01.py
@@ -0,0 +1,59 @@
+"""
+Personal Information Manager 
+
+#Create a tuple to store a person's basic info: (name, age, city, country)
+#Create a list to store their hobbies
+
+Allow the user to:
+
+Display all information
+Add new hobbies
+Remove hobbies
+Update age (by creating a new tuple)
+
+"""
+
+# Complete this program
+def personal_info_manager():
+    # Create initial person tuple
+    person =("Kamonphan",19,"Chonburi","TH") # name, age, city, country
+    hobbies = []
+
+    while True:
+
+        choice = input("What do your want to do ? (1: display, 2: add hobby, 3: remove hobby, 4: edit age ,5: ex:)")
+
+    # Your code here
+        if choice == "1":
+            #display all info
+            print("Neam: ",person[0])
+            print("Age: {person [1]}")
+            print("City:" + person[2])
+            print("Contry: " ,person[3])   
+            print("hobbies: " ,hobbpies )
+
+
+        elif choice == "2":
+            # append hobby
+            hobby = input("What is your new hobbies: ")
+            hobbies.append(hobby)
+
+
+        elif choice == "3":
+        
+            #remove hobby
+            hobbies.pop()
+
+
+        elif choice == "4":
+            # edit age
+            person_list = list(person) #("kamonphan","19","TH")
+            age = input("How old are you ?: ")
+            person_list[1] = age
+            person = tuple(person_list)
+
+        elif choice == "5":
+            break
+
+if __name__ == "__main__":
+    personal_info_manger()
